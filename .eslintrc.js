f9bf9e84839ff886a58200315fa41303f484bef2
Nicolas Ritouet <nicolas@ritouet.com>
2017-07-19 23:44:20 +0200

    eslint + jest

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": [
+        "airbnb-base"
+    ],
+    "env": {
+        "shelljs": true,
+        "node": true
+    },
+    "rules": {
+        "import/no-unresolved": 1,
+        "prefer-const": 1,
+        "no-unused-vars": 1,
+        "no-use-before-define": 1,
+        "one-var": 1,
+        "consistent-return": 1,
+        "no-param-reassign": 1,
+        "global-require": 1,
+        "import/no-dynamic-require": 1
+    }
+}
