c7c2159d78713893bfff09a42ca5eba556e06de0
Nicolas Ritouet <nicolas@ritouet.com>
2017-07-20 23:02:31 +0200

    Add jest and a first test case on CLI

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   ],
   "env": {
     "shelljs": true,
+    "jest": true,
     "node": true
   },
   "rules": {
---
+++ tests/cli.spec.js
@@ -0,0 +1,29 @@
+
+const path = require('path');
+const execa = require('execa');
+const del = require('del');
+
+
+describe('test CLI', () => {
+  beforeEach((done) => {
+    del(['testAppDoNotKeep']);
+    done();
+  });
+
+  it('should create a project', (done) => {
+    execa(path.join(__dirname, '../bin/dpd.js'), ['create', 'testAppDoNotKeep']).then((result) => {
+      console.log(result.stdout);
+      expect(result.stdout).toContain('dpd is installing the dependencies... please be patient (this may take a few minutes)');
+      done();
+    }).catch((err) => {
+      console.log('error', err);
+      done.fail(err);
+    });
+  });
+
+
+  afterEach((done) => {
+    del(['testAppDoNotKeep']);
+    done();
+  });
+});
