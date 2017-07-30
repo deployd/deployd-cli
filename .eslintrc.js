291231678e11cb40660ea38fcf36542012f95380
Kapnobatai3434 <iscetinin@gmail.com>
2017-07-30 15:25:44 +0300

    #16 Fixed eslint errors for "global-require" rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     "one-var": 2, // 8 errors
     "consistent-return": 1, // 8 errors
     "no-param-reassign": 1, // 2 errors
-    "global-require": 1, // 2 errors
+    "global-require": 2, // 2 errors
     "import/no-dynamic-require": 1, // 1 error
     "comma-dangle": ["error", "never"]
   }
