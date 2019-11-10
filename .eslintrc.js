ee1b5f64975fea8be98a15ef896783c36ed99d2a
saloni.kathuria <saloni.kathuria@olx.com>
2019-11-10 22:46:18 +0530

    Issue #33 resolved unused var linting issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,13 +12,13 @@ module.exports = {
     },
   "rules": {
     // TODO: change these rules to errors and fix codebase
-    "prefer-const": 1, // 4 errors
-    "no-unused-vars": 1, // 3 errors
-    "no-use-before-define": 1, // 7 errors
-    "one-var": 1, // 8 errors
-    "consistent-return": 1, // 8 errors
-    "no-param-reassign": 1, // 2 errors
-    "global-require": 1, // 2 errors
+    "prefer-const": "error", // 4 errors
+    "no-unused-vars": "error", // 3 errors
+    "no-use-before-define": "error", // 7 errors
+    "one-var": "error", // 8 errors
+    "consistent-return": "error", // 8 errors
+    "no-param-reassign": "error", // 2 errors
+    "global-require": "error", // 2 errors
     "import/no-dynamic-require": 1, // 1 error
     "comma-dangle": ["error", "never"]
   }
