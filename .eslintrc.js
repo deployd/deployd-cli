43072f80dba566a15f52e2faaf45c405d646b788
saloni.kathuria <saloni.kathuria@olx.com>
2019-11-15 13:04:13 +0530

    Issue #33 removed unnecessary comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,15 +11,14 @@ module.exports = {
        "sourceType": "script"
     },
   "rules": {
-    // TODO: change these rules to errors and fix codebase
-    "prefer-const": "error", // 4 errors
-    "no-unused-vars": "error", // 3 errors
-    "no-use-before-define": "error", // 7 errors
-    "one-var": "error", // 8 errors
-    "consistent-return": "error", // 8 errors
-    "no-param-reassign": "error", // 2 errors
-    "global-require": "error", // 2 errors
-    "import/no-dynamic-require": 1, // 1 error
+    "prefer-const": "error",
+    "no-unused-vars": "error",
+    "no-use-before-define": "error",
+    "one-var": "error",
+    "consistent-return": "error",
+    "no-param-reassign": "error",
+    "global-require": "error",
+    "import/no-dynamic-require": 1,
     "comma-dangle": ["error", "never"]
   }
 }  
